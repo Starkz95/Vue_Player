9b584316a97545d0649b395fa8c16d2a08f769d4
zmc <834176915@qq.com>
2019-12-11 17:57:11 +0800

    UI

---
+++ src/components/player.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="player">
+    <div class="player_disk">
+      <disk></disk>
+    </div>
+    <div class="player_control">
+      <control></control>
+    </div>
+    <div class="player_progress">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import disk from './disk.vue'
+import control from './control.vue'
+export default{
+  components:{
+    disk,
+    control
+  }
+}
+</script>
+
+<style scoped="scoped">
+  .player{
+    margin-top: 30vh;
+    position: relative;
+    display: flex;
+    max-width: 300px;
+    max-height: 75px;
+    width: 60vw;
+    height: 15vw;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: 0 30px 60px rgba(0,0,0,0.12),
+    0 20px 20px rgba(95,23,101,0.2);
+  }
+
+  .player_disk{
+    flex: 1.2;
+    padding: 2.4vw;
+  }
+
+  .player_control{
+    flex: 2;
+    padding: 2.4vw 0;
+    padding-right: 2.4vw;
+  }
+
+  .player_progress{
+    position: absolute;
+    left: 1.8vw;
+    right: 1.8vw;
+    top: 0;
+    bottom: 5vw;
+    z-index: -1;
+  }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <player></player>
+  </div>
+</template>
+
+<script>
+  import player from '../components/player.vue'
+  export default{
+    components:{
+      player
+    }
+  }
+</script>
+
+<style scoped="scoped">
+  .home {
+    position: relative;
+    z-index: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
